46b9d02bcddca1b11c5c84d44ef21d65263f50d5
James Le <le_j6@denison.edu>
2017-08-03 15:54:44 -0700

    Main Menu

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ config.bucket = {
   read_key: '',
   write_key: ''
 }
+
+const MainMenu = () => {
+  inquirer.prompt(
+    {
+      type: 'list',
+      name: 'mainMenu',
+      message: 'Main Menu',
+      choices: [
+        'New Note',
+        'View Notes',
+        new inquirer.Separator(),
+        'Exit'
+      ]
+    }
+  ).then((answers)=>{
+    if(answers.mainMenu === "New Note"){
+      NewNote()
+    } else if(answers.mainMenu === "View Notes"){
+      ViewNotes()
+    } else {
+      process.exit()
+    }
+  })
+}
+MainMenu()
