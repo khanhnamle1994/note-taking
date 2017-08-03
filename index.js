a1d5ed0d1258e6555e104e743cd8baab8c158904
James Le <le_j6@denison.edu>
2017-08-03 15:55:30 -0700

    Adding a New Note

--- index.js
+++ index.js
@@ -34,3 +34,34 @@ const MainMenu = () => {
   })
 }
 MainMenu()
+
+const NewNote = () => {
+  var question = {
+    type: 'input',
+    name: 'note_text',
+    message: 'Note'
+  } // 1
+  inquirer.prompt(question).then((answers) => { // 2
+    var params = {
+      write_key: config.bucket.write_key,
+      type_slug: 'notes',
+      slug: Random.id().toLowerCase(),
+      title: answers.note_text,
+      content: ''
+    } // 3
+    if(answers.note_text === ''){
+      console.log(chalk.red("You cannot add an empty note"))
+      MainMenu()
+      return;
+    }
+    Cosmic.addObject(config, params, (error, response) =>{ // 4
+      if(response.object){
+        console.log(chalk.green("\u2713" + " Success"))
+      }
+      if(error){
+        console.log(error)
+      }
+      MainMenu() //5
+    })
+  })
+}
